b4fb063b10f6b23d6a80bd402099e2ea2b25d128
CassimiroAlbuquerqueIFCE <cassimiro.albuquerque07@aluno.ifce.edu.br>
2024-10-19 13:20:47 -0300

    testando o commit

--- Lab de programacao/lab.c
+++ Lab de programacao/lab.c
@@ -2,7 +2,8 @@
 
 int main(){
     printf("Hellow World!2");
-    return 0; // Esse retorn 0 na função principal quer dizer que, retornando 0, a funçao foi um sucesso
 
+    printf("testenado o github");
+    return 0; // Esse retorn 0 na função principal quer dizer que, retornando 0, a funçao foi um sucesso
 
 }
